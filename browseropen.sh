dd4f297184f3b69371a5596cf2c6ac1421e094dd
arik181 <arik181@gmail.com>
2023-01-30 16:48:04 -0800

    tweak to txt sh. rm opinionated default script. added links for day 1

--- browseropen.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-browser=qutebrowser
-touch `date +%F`
-echo $1 >> /home/arik181/Git/history/`date +%F`
-$browser $1 &
--- browseropen.txt.sh
+++ browseropen.txt.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 browser=w3m
-#browser=qutebrowser
 touch `date +%F`
 echo $1 >> /home/arik181/Git/history/`date +%F`
 zellij --session snownews r -c -- $browser $1
