c80ac6e6b62576a193fd0409cbd4d0d096c0bfa7
arik181 <arik181@gmail.com>
2023-01-31 08:22:55 -0800

    checking into git on txt version

--- browseropen.txt.sh
+++ browseropen.txt.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 browser=w3m
-touch `date +%F`
-echo $1 >> /home/arik181/Git/history/`date +%F`
+dt=`date +%F`
+touch $dt
+echo $1 >> /home/arik181/Git/history/${dt}
 zellij --session snownews r -c -- $browser $1
+cd /home/arik181/Git/history
+git add * 
+git commit -m "Adding links for ${dt}"
